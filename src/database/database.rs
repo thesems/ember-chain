39c79843eebc2b9d70f83f8f9010b43f52fceac1
Semir Ramovic <semir.ramovic@outlook.com>
2024-03-25 22:19:18 +0100

    feat: add utxo to database

--- src/database/database.rs
+++ src/database/database.rs
@@ -6,4 +6,13 @@ pub trait Database {
 
     /// Method to retrieve the blockchain from the database
     fn get_blocks(&self) -> &[Block];
+
+    /// Method to add a new unspent transaction output (UTXO) to the database
+    fn add_utxo(&mut self, tx_hash: String, output_index: usize);
+
+    /// Method to remove a spent transaction output (UTXO) from the database
+    fn remove_utxo(&mut self, tx_hash: &str, output_index: usize);
+
+    /// Method to check if a transaction output is unspent
+    fn is_utxo(&self, tx_hash: &str, output_index: usize) -> bool;
 }
--- src/database/in_memory.rs
+++ src/database/in_memory.rs
@@ -1,14 +1,20 @@
+use std::collections::HashSet;
+
 use crate::block::Block;
 
 use super::database::Database;
 
 pub struct InMemoryDatabase {
     blocks: Vec<Block>,
+    unspent_outputs: HashSet<(String, usize)>,
 }
 
 impl InMemoryDatabase {
     fn new() -> Self {
-        InMemoryDatabase { blocks: vec![] }
+        InMemoryDatabase {
+            blocks: vec![],
+            unspent_outputs: HashSet::new(),
+        }
     }
 }
 
@@ -20,16 +26,33 @@ impl Database for InMemoryDatabase {
     fn get_blocks(&self) -> &[Block] {
         &self.blocks
     }
+
+    fn add_utxo(&mut self, tx_hash: String, output_index: usize) {
+        self.unspent_outputs.insert((tx_hash, output_index));
+    }
+
+    fn remove_utxo(&mut self, tx_hash: &str, output_index: usize) {
+        self.unspent_outputs
+            .remove(&(tx_hash.to_string(), output_index));
+    }
+
+    fn is_utxo(&self, tx_hash: &str, output_index: usize) -> bool {
+        self.unspent_outputs
+            .contains(&(tx_hash.to_string(), output_index))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{block::{Block, BlockHeader}, database::{database::Database, InMemoryDatabase}};
+    use crate::{
+        block::Block,
+        database::{database::Database, InMemoryDatabase},
+    };
 
     #[test]
     fn test_insert_block() {
         let mut in_memory_db = InMemoryDatabase::new();
-        in_memory_db.insert_block(Block::new(BlockHeader::default(), vec![]));
+        in_memory_db.insert_block(Block::default());
         assert_eq!(in_memory_db.blocks.len(), 1)
     }
 }
